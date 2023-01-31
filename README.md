a1af1aedc98e2327ad6bf23295999b4b401f515b
akil <bhuiyanakil@gmail.com>
2023-01-30 21:36:11 -0500

    v1

--- README.md
+++ README.md
@@ -1,44 +1,38 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *GoodFund*
 
-Submitted by: **Your Name Here**
+Submitted by: **Akil Adnan Bhuiyan**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**GoodFund** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **18** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [x] The introduction section explains the background of the company and how many games remain unfunded.
+* [x] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [x] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [x] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
 
-* [ ] List anything else that you can get done to improve the app functionality!
+* [x] List anything else that you can get done to improve the app functionality!
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='assets/3.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
-<!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
 
 ## Notes
 
-Describe any challenges encountered while building the app.
+Creating, adding, implementing functionality and design was very straigtforward. Its definitely refreshed my memory of html, css and js. I had to research about filter and reduce methods, but afterward I realized the benefits of it, which make the website more efficient. In this prework, I learned JavaScript features such as template literals, functions, filter, map, reduce, destructuring and spread operators, logic evaluation. 
 
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright [2023] [Akil Adnan Bhuiyan]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const GAMES_JSON = JSON.parse(GAMES_DATA)
 // remove all child elements from a parent element in the DOM
 function deleteChildElements(parent) {
     while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+        parent.removeChild(parent.firstChild); 
     }
 }
 
@@ -29,25 +29,34 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
-        // create a new div element, which will become the game card
-
+    for(var i=0; i < games.length; i++){
+       // create a new div element, which will become the game card
+        var newDiv = document.createElement("div")
 
         // add the class game-card to the list
-
+        newDiv.classList.add("game-card")
 
         // set the inner HTML using a template literal to display some info 
-        // about each game
+        // about each game  
+
+        var img = document.createElement("img")
+        img.classList.add("game-img")
+        img.src =  games[i]["img"]
+        newDiv.innerHTML= `<b> ${games[i]["name"]}</b><br><br> ${games[i]["description"]}<br><br> Backers: ${games[i]["backers"]}<br><br>`
+     
+        // append the game to the games-container
+        newDiv.appendChild(img)
+        gamesContainer.append(newDiv)
+        //document.write(gamesContainer.innerHTML)
+    }        
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
-        // append the game to the games-container
-
 }
 
 // call the function we just defined using the correct variable
+addGamesToPage(GAMES_JSON)
+
+
 // later, we'll call this function using a different list of games
 
 
@@ -61,19 +70,27 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+let count = GAMES_JSON.reduce(function(sum, currValue){
+    return sum + currValue.backers
+}, 0)
 
-
+//console.log(count)
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.append(count.toLocaleString())
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+let count2 = GAMES_JSON.reduce(function(sum, currValue){
+    return sum + currValue.pledged
+}, 0)
 
 // set inner HTML using template literal
-
+raisedCard.append(`$` + count2.toLocaleString())
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+let count3 = GAMES_JSON.length
+gamesCard.append(count3)
 
 
 /*************************************************************************************
@@ -87,30 +104,43 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const cond1 = GAMES_JSON.filter(function(obj)
+    {
+        return obj.pledged < obj.goal
+    })
+    //console.log(cond1)
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(cond1)
 }
+filterUnfundedOnly()
 
 // show only games that are fully funded
 function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const cond2 = GAMES_JSON.filter(function(obj)
+    {
+        return obj.pledged >= obj.goal
+    })
+    //console.log(cond2)
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(cond2)
 }
+filterFundedOnly()
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON)
 
+    //console.log(GAMES_JSON)
 }
+showAllGames()
 
 // select each button in the "Our Games" section
 const unfundedBtn = document.getElementById("unfunded-btn");
@@ -118,7 +148,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly)
+fundedBtn.addEventListener("click", filterFundedOnly)
+allBtn.addEventListener("click", showAllGames)
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,12 +161,18 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const count4 = GAMES_JSON.filter(item => item.pledged < item.goal).length;
+    //console.log(count4)
 
-
-// create a string that explains the number of unfunded games using the ternary operator
-
+// create a string that explains the number of unfunded games using the ternary operator   
+var displayString =
+ `A  total of $` + count2.toLocaleString() + ` has been raised for `+ count3 + ` ${count3 > 1 ? "games": "game"}. Currently, ` + count4 + ` ${count4 > 1 ? "games remain": "game remains"}  unfunded. We need your help to fund these amazing games!`
+//console.log(displayString)
 
 // create a new DOM element containing the template string and append it to the description container
+var p = document.createElement("p");
+p.append(displayString)
+descriptionContainer.append(p)
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -146,10 +184,38 @@ const secondGameContainer = document.getElementById("second-game");
 
 const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
     return item2.pledged - item1.pledged;
+    
 });
+//console.log(sortedGames)
 
 // use destructuring and the spread operator to grab the first and second games
+let {name: name1, description: desc1, pledged: pl1, goal: goal1, backers: bkrs1, img: img1} = sortedGames[0];
+let {name: name2, description: desc2, pledged: pl2, goal: goal2, backers: bkrs2, img: img2} = sortedGames[1];
+
+//console.log(name1)
+const first = name1;
+const second = name2;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+var topGame = document.createElement("div")
+topGame.append(first)
+firstGameContainer.append(topGame)
+// do the same for the runner up it
+var runnerUp = document.createElement("div")
+runnerUp.append(second)
+secondGameContainer.append(runnerUp)
 
-// do the same for the runner up item
+//change Theme
+let themeToggler = document.querySelector('.theme-toggler');
+
+themeToggler.onclick = () =>{
+
+    themeToggler.classList.toggle('active');
+
+    if(themeToggler.classList.contains('active')){
+        document.body.classList.add('active');
+    }
+    else{
+        document.body.classList.remove('active');
+    }
+}
